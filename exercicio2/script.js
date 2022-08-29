0d0188a34da3ab4462d1d6651bc743d2001f02ab
MarianaMeijon <marianameijon1989@gmail.com>
2022-08-29 18:39:02 -0300

    Exercício 1

---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+let vcgostadebolo = "Você gosta de bolo?"
+let bolo = prompt (vcgostadebolo)
+console.log (bolo)
+let vcgostadecafe = "Você gosta de café?"
+let cafe = prompt (vcgostadecafe)
+console.log (cafe)
+let tachovendo = "Está chovendo hoje?"
+let chuva = prompt (tachovendo)
+console.log (chuva)
+
+console.log (vcgostadebolo, bolo)
+console.log (vcgostadecafe, cafe)
+console.log (tachovendo, chuva)
